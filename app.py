34d314bad87cf79fc93156134ee5a3bf902d6ee9
KRISH PARMAR <krishparmarrollno@gmail.com>
2026-03-20 19:53:44 +0530

    Change secret key to use environment variable

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ for _pkg in ("punkt", "punkt_tab", "stopwords"):
         pass
 
 app = Flask(__name__)
-app.secret_key = "krish_secret_key_2025"
+app.secret_key = os.getenv("SECRET_KEY")
 
 TRUSTED_CHANNELS = {
     "Khan Academy",
